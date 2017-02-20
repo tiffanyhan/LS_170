7fa3de9da214a26adc5be5f0f73b8be1e161066d
Tiffany Han <hantiffany@gmail.com>
2017-02-20 10:47:18 -0800

    send a complete http response in server.rb

--- lesson3/server.rb
+++ lesson3/server.rb
@@ -17,23 +17,31 @@ loop do
   client = server.accept
   request_line = client.gets
   next if !request_line || request_line =~ /favicon/
-
-  puts request_line
-  # GET /?rolls=2&sides=6 HTTP/1.1
   http_method, path, params = parse_request(request_line)
 
-  puts http_method
-  puts path
-  puts params
+  client.puts 'HTTP/1.0 200 OK'
+  client.puts 'Content-Type: text/html'
+  client.puts
+  client.puts '<html>'
+  client.puts '<body>'
+  client.puts '<pre>'
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts '</pre>'
+
+  client.puts '<h1>Rolls!</h1>'
 
   rolls = params['rolls'].to_i
   sides = params['sides'].to_i
 
   rolls.times do
     roll = rand(sides) + 1
-    puts roll
+    client.puts '<p>', roll, '</p>'
   end
 
-  client.puts 'hello'
+  client.puts '</body>'
+  client.puts '</html>'
+
   client.close
 end
